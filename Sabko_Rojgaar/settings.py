51ffd1c2d378b2617c934917622a9bb12c06fa0b
Kajol Khairnar <74052505+kajolkhairnar@users.noreply.github.com>
2021-11-20 08:44:29 +0530

    Update settings.py

--- Sabko_Rojgaar/settings.py
+++ Sabko_Rojgaar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://sabko-rojgaar.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = [default='sabko-rojgaar.herokuapp.com','localhost']
 
 
 # Application definition
