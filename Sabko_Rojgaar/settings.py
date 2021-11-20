87ce55c70136ba625427ad9e79776c533263462b
Kajol Khairnar <74052505+kajolkhairnar@users.noreply.github.com>
2021-11-20 06:28:48 +0530

    Update settings.py

--- Sabko_Rojgaar/settings.py
+++ Sabko_Rojgaar/settings.py
@@ -82,9 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
@@ -124,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=BASE_DIR/'static'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
